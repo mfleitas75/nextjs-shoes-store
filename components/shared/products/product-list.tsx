71797c865b1ac501eb9d081d685d9ba38bcd5fb7
mfleitas75 <m.fleitas33064@gmail.com>
2025-03-01 10:48:43 -0500

    ZOD Schema Validation and Types

--- components/shared/products/product-list.tsx
+++ components/shared/products/product-list.tsx
@@ -1,6 +1,7 @@
+import { Product } from "@/types";
 import ProductCard from "./product-card";
 
-const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?: number }) => {
+const ProductList = ({ data, title, limit }: { data: Product[]; title?: string; limit?: number }) => {
     const limitData = limit ? data.slice(0, limit) : data;
 
   return (
@@ -8,7 +9,7 @@ const ProductList = ({ data, title, limit }: { data: any; title?: string; limit?
       <h2 className="h2-bold mb-4">{title}</h2>
       {limitData.length > 0 ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {limitData.map((product: any, index: number) => (
+          {limitData.map((product: Product, index: number) => (
             <ProductCard key={index} product={product} />
           ))}
         </div>
