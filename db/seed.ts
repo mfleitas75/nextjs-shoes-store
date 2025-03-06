1e5ffdf654125c00fd2a9817533e60d56e192675
mfleitas75 <m.fleitas33064@gmail.com>
2025-03-05 21:39:44 -0500

    User Model and Seed User Data

--- db/seed.ts
+++ db/seed.ts
@@ -2,9 +2,10 @@ import sampleData from './sample-data';
 import { prisma } from './prisma';
 
 async function main() {
-    await prisma.product.deleteMany();
-
-    await prisma.product.createMany({ data: sampleData.products });
+    await prisma.user.deleteMany();
+    await prisma.user.createMany({ data: sampleData.users });
+    // await prisma.product.deleteMany();
+    // await prisma.product.createMany({ data: sampleData.products });
 
     console.log("Database seeded successfully!");
 }
